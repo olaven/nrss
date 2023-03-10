f48b6f9db5057237c0581fbc570e2aef94fd4cdc
olaven <olav@sundfoer.com>
2023-03-10 14:05:50 +0100

    typo - thanks @aridder <3

--- routes/index.tsx
+++ routes/index.tsx
@@ -40,7 +40,7 @@ export default function Home({ data }: PageProps<HandlerData>) {
                     </div>
                 ) : null}
                 <h2 className="text-3xl">Hva er dette?</h2>
-                <p>Hei! Denne løsningen er laget som en reaksjon på at statsfinnansierte NRK lukker ned innholdet sitt til sin egen app fremfor å bygge oppunder åpne standarder som RSS. Denne siden er laget i dag (09.03.23), fort og gæli - den er ustabil og kommer til å krasje :) Sjekk gjerne ut <a href="https://github.com/olaven/NRSS/" className="underline">kildekoden</a>. </p>
+                <p>Hei! Denne løsningen er laget som en reaksjon på at statsfinansierte NRK lukker ned innholdet sitt til sin egen app fremfor å bygge oppunder åpne standarder som RSS. Denne siden er laget i dag (09.03.23), fort og gæli - den er ustabil og kommer til å krasje :) Sjekk gjerne ut <a href="https://github.com/olaven/NRSS/" className="underline">kildekoden</a>. </p>
                 <h2 className="text-3xl">Hvordan bruker jeg dette?</h2>
                 <p>Søk på NRK-podcasten du vil høre på. Kopier deretter URL-en under bildet. Lim denne inn i akkurat den podcastspilleren du måtte foretrekke!</p>
             </div>
