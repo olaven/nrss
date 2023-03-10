2c74cdab3f5b58121dc97c6fec3bb18a59b3fa62
August Skare <post@augustskare.no>
2023-03-10 13:21:44 +0100

    Move search to server (#2)

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,28 @@
 import { Head } from "$fresh/runtime.ts";
-import { useState } from "preact/hooks";
-import { post, OK } from "https://deno.land/x/kall@v0.1.0/mod.ts";
-import Search from "../islands/Search.tsx"
+import { Handlers, PageProps } from "$fresh/server.ts";
+import Search from "../components/Search.tsx"
+import { SerieCard } from "../components/SeriesCard.tsx";
+import { nrkRadio, Serie } from "../lib/nrk.ts";
 
-export default function Home() {
+interface HandlerData {
+    query: string;
+    result?: Serie[];
+    origin: string;
+}
 
+export const handler: Handlers<HandlerData> = {
+    async GET(request, ctx) {
+        const url = new URL(request.url);
+        const query = url.searchParams.get("query");
+        let result: Serie[] | undefined;
+        if (query) {
+            result = await nrkRadio.search(query);
+        }
+        return ctx.render({ query: query || "", result, origin: url.origin });
+    },
+  };
+
+export default function Home({ data }: PageProps<HandlerData>) {
     return (
         <>
             <Head>
@@ -12,7 +30,12 @@ export default function Home() {
             </Head>
             <div className="p-4 mx-auto max-w-screen-md bg-blue-400">
                 <h1 className="text-4xl text-center">NRSS - NRK-podcast som RSS</h1>
-                <Search />
+                <Search defaultValue={data.query} />
+                {data.result ? (
+                    <div className="w-1/2 mx-auto my-4">   
+                        {data.result.map(result => <SerieCard serie={result} origin={data.origin} />)}
+                    </div>
+                ) : null}
                 <h2 className="text-3xl">Hva er dette?</h2>
                 <p>Hei! Denne løsningen er laget som en reaksjon på at statsfinnansierte NRK lukker ned innholdet sitt til sin egen app fremfor å bygge oppunder åpne standarder som RSS. Denne siden er laget i dag (09.03.23), fort og gæli - den er ustabil og kommer til å krasje :) Sjekk gjerne ut <a href="https://github.com/olaven/NRSS/" className="underline">kildekoden</a>. </p>
                 <h2 className="text-3xl">Hvordan bruker jeg dette?</h2>
