3e23bf443e8e3a666c3cc55166a0293fa31c1560
olaven <olav@sundfoer.com>
2023-03-16 22:11:56 +0100

    Chapters (from podcast 2.0 spec)

--- lib/nrk.ts
+++ lib/nrk.ts
@@ -6,7 +6,7 @@ import { components as catalogComponents } from './nrk-catalog.ts';
 type ArrayElement<A> = A extends readonly (infer T)[] ? T : never
 export type Serie = catalogComponents["schemas"]["SeriesViewModel"]
 type OriginalEpisode = catalogComponents["schemas"]["EpisodeHalResource"];
-export type Episode = OriginalEpisode & { url: string; }
+export type OriginalEpisode = OriginalEpisode & { url: string; }
 export type SearchResultList = searchComponents["schemas"]["seriesResult"]["results"];
 export type SearchResult = ArrayElement<SearchResultList> & { description?: string }
 
@@ -25,7 +25,7 @@ interface Manifest {
     };
 }
 
-async function withDownloadLink(episode: OriginalEpisode): Promise<Episode> {
+async function withDownloadLink(episode: OriginalEpisode): Promise<OriginalEpisode> {
     // getting stream link
     const [playbackStatus, playbackResponse] = await get<Manifest>(`https://psapi.nrk.no/playback/manifest/podcast/${episode.episodeId}`)
 
@@ -67,5 +67,16 @@ export const nrkRadio = {
             throw `Error getting episodes for ${seriesId}: EpisodeStatus: ${episodeStatus}. SerieStatus: ${seriesStatus}`
         }
 
+    },
+    getEpisode: async (seriesId: string, episodeId: string): Promise<OriginalEpisode> => {
+
+
+        const url = `https://psapi.nrk.no/radio/catalog/podcast/${seriesId}/episodes/${episodeId}`
+        const [status, episode] = await get<OriginalEpisode>(url);
+        if (status !== OK) {
+            throw `Error getting episode ${episodeId}. Status: ${status}. Series: ${seriesId}`;
+        }
+
+        return episode;
     }
 }
---
+++ routes/api/feeds/[seriesId]/[episodeId]/chapters.ts
@@ -0,0 +1,23 @@
+import { parse, toSeconds } from "https://esm.sh/iso8601-duration@2.1.1";
+import { nrkRadio, OriginalEpisode } from "../../../../../lib/nrk.ts";
+
+function toChapters(episode: OriginalEpisode) {
+    return episode.indexPoints.map(indexPoint => ({
+        title: indexPoint.title,
+        startTime: toSeconds(parse(indexPoint.startPoint))
+    }))
+}
+export const handler = async (req: Request, _ctx: HandlerContext): Promise<Response> => {
+    const seriesId = _ctx.params.seriesId;
+    const episodeId = _ctx.params.episodeId;
+
+    console.log("going to get ep for ", episodeId);
+    const episode = await nrkRadio.getEpisode(seriesId, episodeId);
+    const chapters = toChapters(episode);
+
+    return new Response(JSON.stringify(chapters), {
+        headers: {
+            "Content-Type": "application/json+chapters"
+        }
+    });
+};
