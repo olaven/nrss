b66022297f0e6e7a0156367bbe5cf4db5764e714
Tim Hårek Andreassen <tim@harek.no>
2024-03-02 18:45:09 +0100

    feat: Add URL beside copy button
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- components/SeriesCard.tsx
+++ components/SeriesCard.tsx
@@ -6,10 +6,11 @@ export default function SeriesCard(props: { serie: SearchResult; origin: string 
   const image = props.serie.images[0];
   return (
     <div className="border-2 my-2 p-2">
-      <div className="mx-auto h-full w-full">
+      <div className="mx-auto h-full w-full space-y-2">
         <h2 className="text-xl">{props.serie.title}</h2>
         <p className="text-md">{props.serie.description}</p>
         <img src={image.uri} width={image.width} />
+        <code className="font-mono bg-black text-white select-all p-2">{feedUrl.toString()}</code>
         <CopyButton text={feedUrl.toString()}>
           Kopier URL
         </CopyButton>
