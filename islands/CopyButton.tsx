ee8956bc5376a7bcd505ab4887f9f6e462708e89
Tim Hårek Andreassen <tim@harek.no>
2024-03-02 18:25:22 +0100

    fix: Clean up types
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- islands/CopyButton.tsx
+++ islands/CopyButton.tsx
@@ -2,9 +2,13 @@ import Preact from "preact";
 import { useState } from "preact/hooks";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
-export default function CopyButton(
-  props: { textBefore: string; textAfter: string } & Preact.PropsWithChildren,
-) {
+type Props = {
+  text: string;
+  children: Preact.ComponentChildren;
+  disabled?: boolean;
+};
+
+export default function CopyButton({ text, disabled = false, children }: Props) {
   const [clicked, setClicked] = useState(false);
 
   const startReset = () => {
@@ -16,14 +20,14 @@ export default function CopyButton(
   return (
     <button
       onClick={() => {
-        navigator.clipboard.writeText(props.text.toString());
+        navigator.clipboard.writeText(text.toString());
         setClicked(true);
         startReset();
       }}
-      disabled={!IS_BROWSER || props.disabled}
+      disabled={!IS_BROWSER || disabled}
       class="px-2 py-1 border(gray-100 2) hover:bg-gray-200 mx-auto"
     >
-      {clicked ? "Kopiert!" : props.children}
+      {clicked ? "Kopiert!" : children}
     </button>
   );
 }
