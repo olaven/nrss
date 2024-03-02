9b59721304fe7c6ff39f363ceae8a64e1348ac39
Tim Hårek Andreassen <tim@harek.no>
2024-03-02 18:07:56 +0100

    chore: Remove old type imports with `<reference>`
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
-/// <reference lib="dom.asynciterable" />
-/// <reference lib="deno.ns" />
-
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
