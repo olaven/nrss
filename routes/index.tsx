39a54505c8087f03b0e2849de99ab4fe596b20bf
Tim Hårek Andreassen <tim@harek.no>
2024-03-30 16:14:44 +0100

    feat: Add markdown-rendering support for quick docs
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,11 +5,13 @@ import Header from "../components/Header.tsx";
 import Search from "../components/Search.tsx";
 import SeriesCard from "../components/SeriesCard.tsx";
 import { nrkRadio, SearchResultList } from "../lib/nrk.ts";
+import { CSS, render } from "$gfm";
 
 interface HandlerData {
   query: string;
-  result?: SearchResultList;
   origin: string;
+  rawMarkdown: string;
+  result?: SearchResultList;
 }
 
 export const handler: Handlers<HandlerData> = {
@@ -20,7 +22,8 @@ export const handler: Handlers<HandlerData> = {
     if (query) {
       result = await nrkRadio.search(query);
     }
-    return ctx.render({ query: query || "", result, origin: url.origin });
+    const rawMarkdown = await Deno.readTextFile(new URL("../docs/what.md", import.meta.url));
+    return ctx.render({ query: query || "", result, origin: url.origin, rawMarkdown });
   },
 };
 
@@ -29,6 +32,7 @@ export default function Home({ data }: PageProps<HandlerData>) {
     <>
       <Head>
         <title>{data.query ? `Søk: ${data.query} - ` : ""}NRSS</title>
+        <style dangerouslySetInnerHTML={{ __html: CSS }} />
       </Head>
       <div className="p-4 mx-auto max-w-screen-md">
         <Header />
@@ -45,30 +49,10 @@ export default function Home({ data }: PageProps<HandlerData>) {
             </div>
           )
           : null}
-        <h2 className="text-3xl">Hva er dette?</h2>
-        <p>
-          Hei! Denne løsningen er laget som en reaksjon på at statsfinansierte NRK lukker ned innholdet sitt til sin
-          egen app fremfor å bygge oppunder åpne standarder som RSS. Denne siden er laget fort og gæli - den er ustabil
-          og kommer til å krasje :) Sjekk gjerne ut{" "}
-          <a
-            href="https://github.com/olaven/NRSS/"
-            className="underline"
-          >
-            kildekoden
-          </a>.
-        </p>
-        <h2 className="text-3xl">Hvordan bruker jeg dette?</h2>
-        <p>
-          Søk på NRK-podcasten du vil høre på. Kopier deretter URL-en under bildet. Lim denne inn i akkurat den
-          podcastspilleren du måtte foretrekke! Se{" "}
-          <a
-            className="underline"
-            href="https://help.omnystudio.com/en/articles/5222518-podcast-apps-that-support-add-rss-feed"
-          >
-            her
-          </a>{" "}
-          for en oversikt over hvordan det gjøres i populære podcastspillere.
-        </p>
+        <div
+          class="markdown-body"
+          dangerouslySetInnerHTML={{ __html: render(data?.rawMarkdown) }}
+        />
         <Footer />
       </div>
     </>
