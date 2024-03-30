9c2bd61f7dbac7e3a4a7cd3bd22502bfd66d074e
Tim Hårek Andreassen <tim@harek.no>
2024-03-30 16:35:13 +0100

    style: Make title bigger
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="text-center">
-      <h1 className="text-2xl lg:text-3xl font-semibold">
+      <h1 className="text-4xl lg:text-3xl font-semibold">
         <a href="/">📻 NRSS</a>
       </h1>
       <h2 className="lg:text-xl">NRK-podcast som RSS</h2>
