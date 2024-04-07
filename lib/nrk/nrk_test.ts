0434448d45acb2e5056c146e041704004fadd887
Tim Hårek Andreassen <tim@harek.no>
2024-04-07 22:33:56 +0200

    chore: Formatting
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- lib/nrk/nrk_test.ts
+++ lib/nrk/nrk_test.ts
@@ -5,7 +5,6 @@ import { assertExists } from "https://deno.land/std@0.216.0/assert/assert_exists
 
 Deno.test("Verify search query `trygd` returns one result: 'Trygdekontoret'", async () => {
   const result = await nrkRadio.search("trygd");
-
   assertExists(result);
   assertEquals(result.length, 1);
   assertEquals(result[0].seriesId, "trygdekontoret");
@@ -18,7 +17,6 @@ Deno.test("Verify empty search query yields `null`", async () => {
 
 Deno.test("Verify getting series data for 'trygdekontoret' works", async () => {
   const result = await nrkRadio.getSeries("trygdekontoret");
-
   assertExists(result);
   assertGreaterOrEqual(result.episodes.length, 1);
 });
@@ -30,13 +28,11 @@ Deno.test("Verify getting series data for 'trygd' does not works", async () => {
 
 Deno.test("Verify getting episodeId 'l_0bc5e55a-46b5-48a5-85e5-5a46b5d8a562' for 'trygdekontoret' works", async () => {
   const result = await nrkRadio.getEpisode("trygdekontoret", "l_0bc5e55a-46b5-48a5-85e5-5a46b5d8a562");
-
   assertExists(result);
   assertEquals(result.duration.seconds, 4152);
 });
 
 Deno.test("Verify getting episodeId 'null' for 'trygdekontoret' yields `null`", async () => {
   const result = await nrkRadio.getEpisode("trygdekontoret", "null");
-
   assertEquals(result, null);
 });
