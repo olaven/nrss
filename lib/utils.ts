39c139be7b73ef3c8d69df7bec4b2f19ab782774
Tim Hårek Andreassen <tim@harek.no>
2024-04-08 18:46:04 +0200

    refactor: Make response helpers for JSON and XML
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { STATUS_CODE } from "$fresh/server.ts";
+
 export function getHostName() {
   const deploymentId = Deno.env.get("DENO_DEPLOYMENT_ID");
   if (deploymentId) {
@@ -6,4 +8,24 @@ export function getHostName() {
     // assume env
     return "http://localhost:8000";
   }
+}
+
+type EnumValues<T> = T[keyof T];
+type Status = EnumValues<typeof STATUS_CODE>;
+
+export function responseJSON(body: unknown | null, status: Status) {
+  return response(body, status, "json");
+}
+
+export function responseXML(body: unknown | null, status: Status) {
+  return response(body, status, "xml");
+}
+
+function response(body: unknown | null, status: number, type: "json" | "xml") {
+  return new Response(JSON.stringify(body), {
+    status,
+    headers: {
+      "Content-Type": `application/${type}`,
+    },
+  });
 }
--- routes/api/feeds/[seriesId].ts
+++ routes/api/feeds/[seriesId].ts
@@ -1,24 +1,16 @@
 import { FreshContext, STATUS_CODE } from "$fresh/server.ts";
 import { caching } from "../../../lib/caching.ts";
 import { rss } from "../../../lib/rss.ts";
+import { responseJSON, responseXML } from "../../../lib/utils.ts";
 
 export const handler = async (_req: Request, ctx: FreshContext): Promise<Response> => {
   const seriesId = ctx.params.seriesId;
 
   const series = await caching.getSeries({ id: seriesId });
   if (!series) {
-    return new Response(JSON.stringify({ message: "Series not found" }), {
-      status: STATUS_CODE.NotFound,
-      headers: {
-        "Content-Type": "application/xml",
-      },
-    });
+    return responseJSON({ message: "Series not found" }, STATUS_CODE.NotFound);
   }
   const feed = rss.assembleFeed(series);
 
-  return new Response(feed, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
+  return responseXML(feed, STATUS_CODE.OK);
 };
