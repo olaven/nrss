11413d6d9403adf106c995f7150e6bd4cfb441a9
Tim Hårek Andreassen <tim@harek.no>
2024-04-12 14:54:27 +0200

    fix: Make all network requests go through for Fresh server (#28)

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-import twindPlugin from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./twind.config.ts";
-
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, config);
