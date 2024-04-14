olaven/nrss

commit 33dbdfefcc62916bf22ce8367561b6b73f1c8cab

feat: Add unit-tests (#25)

lib/utils.ts
@@ -1,0 +1,31 @@
+import { STATUS_CODE } from "$fresh/server.ts";
+
+export function getHostName() {
+  const deploymentId = Deno.env.get("DENO_DEPLOYMENT_ID");
+  if (deploymentId) {
+    return `https://nrss-${deploymentId}.deno.dev`;
+  } else {
+    // assume env
+    return "http://localhost:8000";
+  }
+}
+
+type EnumValues<T> = T[keyof T];
+type Status = EnumValues<typeof STATUS_CODE>;
+
+export function responseJSON(body: unknown | null, status: Status) {
+  return response(body, status, "json");
+}
+
+export function responseXML(body: unknown | null, status: Status) {
+  return response(body, status, "xml");
+}
+
+function response(body: unknown | null, status: number, type: "json" | "xml") {
+  return new Response(JSON.stringify(body), {
+    status,
+    headers: {
+      "Content-Type": `application/${type}`,
+    },
+  });
+}

routes/api/feeds/[seriesId].ts
@@ -1,16 +1,16 @@
-import { FreshContext } from "$fresh/server.ts";
+import { FreshContext, STATUS_CODE } from "$fresh/server.ts";
 import { caching } from "../../../lib/caching.ts";
 import { rss } from "../../../lib/rss.ts";
+import { responseJSON, responseXML } from "../../../lib/utils.ts";
 
 export const handler = async (_req: Request, ctx: FreshContext): Promise<Response> => {
   const seriesId = ctx.params.seriesId;
 
   const series = await caching.getSeries({ id: seriesId });
+  if (!series) {
+    return responseJSON({ message: "Series not found" }, STATUS_CODE.NotFound);
+  }
   const feed = rss.assembleFeed(series);
 
-  return new Response(feed, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
+  return responseXML(feed, STATUS_CODE.OK);
 };

utils.ts
@@ -1,9 +1,0 @@
-export function getHostName() {
-  const deploymentId = Deno.env.get("DENO_DEPLOYMENT_ID");
-  if (deploymentId) {
-    return `https://nrss-${deploymentId}.deno.dev`;
-  } else {
-    // assume env
-    return "http://localhost:8000";
-  }
-}

.github/workflows/test.yaml
@@ -1,0 +1,18 @@
+name: Test
+
+on:
+  push:
+
+jobs:
+  run-tests:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@master
+      - name: Setup deno
+        uses: denoland/setup-deno@v1
+        with:
+          deno-version: v1.x
+      - name: Run checks
+        run: deno task check
+      - name: Run test
+        run: deno task test

components/Search.tsx
@@ -1,4 +1,4 @@
-export default function Search(props: { defaultValue: string }) {
+export default function Search(props: { defaultValue: string | null }) {
   return (
     <form className="flex flex-col w-full lg:w-2/3 mx-auto my-4">
       <label className="sr-only" htmlFor="query">Program</label>
@@ -8,7 +8,7 @@ export default function Search(props: { defaultValue: string }) {
         className="border-2 rounded-md px-2"
         name="query"
         id="query"
-        defaultValue={props.defaultValue}
+        defaultValue={props.defaultValue ?? ""}
       />
       <button type="submit" className="my-2 bg-gray-100 border-2 border-gray-400 rounded-md">
         Søk

generate-types.ts
@@ -2,15 +2,15 @@ import openapiTS from "openapi-typescript";
 
 const nrkTypes: { filename: string; url: URL }[] = [
   {
-    filename: "./lib/nrk-search.ts",
+    filename: "./lib/nrk/nrk-search.ts",
     url: new URL("https://psapi.nrk.no/documentation/openapi/search-radio/v1/openapi.yml"),
   },
   {
-    filename: "./lib/nrk-catalog.ts",
+    filename: "./lib/nrk/nrk-catalog.ts",
     url: new URL("https://psapi.nrk.no/documentation/openapi/programsider-radio/openapi.yml"),
   },
   {
-    filename: "./lib/nrk-playback.ts",
+    filename: "./lib/nrk/nrk-playback.ts",
     url: new URL("https://psapi.nrk.no/documentation/openapi/playback/2.0/openapi.json"),
   },
 ] as const;

lib/caching.test.ts
@@ -1,0 +1,51 @@
+import { assertEquals } from "$std/assert/mod.ts";
+import { forTestingOnly } from "./caching.ts";
+import { testUtils } from "./test-utils.ts";
+
+Deno.test("Verify getTimeSinceLastFetch for correct difference", () => {
+  const earlyDate = new Date("2024-04-07T20:24:32Z");
+  const date = new Date("2024-04-07T23:24:32Z");
+  const timeSinceLastFetch = forTestingOnly.getTimeSinceLastFetch(date, earlyDate);
+
+  assertEquals(timeSinceLastFetch, 3);
+});
+
+Deno.test("Verify getTimeSinceLastFetch for bad date", () => {
+  const earlyDate = new Date("2024-04-07T20:24:32Z");
+  const date = new Date("bad date");
+  const timeSinceLastFetch = forTestingOnly.getTimeSinceLastFetch(date, earlyDate);
+
+  assertEquals(timeSinceLastFetch, NaN);
+});
+
+Deno.test("Verify getTimeSinceLastFetch for bad dates (plural)", () => {
+  const earlyDate = new Date("badest date");
+  const date = new Date("bad date");
+  const timeSinceLastFetch = forTestingOnly.getTimeSinceLastFetch(date, earlyDate);
+
+  assertEquals(timeSinceLastFetch, NaN);
+});
+
+Deno.test("Verify seriesFromStorage is old", () => {
+  const earlyDate = new Date("2024-04-07T21:24:32Z");
+  const date = new Date("2024-04-07T23:24:32Z");
+  const timeSinceLastFetch = forTestingOnly.getTimeSinceLastFetch(date, earlyDate);
+
+  const series = testUtils.generateSeries({ lastFetchedAt: date });
+  const syncInterval = 2;
+  const isNew = forTestingOnly.isSeriesFromStorageNew(series, syncInterval, timeSinceLastFetch);
+
+  assertEquals(isNew, true);
+});
+
+Deno.test("Verify seriesFromStorage is new", () => {
+  const earlyDate = new Date("2024-04-07T20:24:32Z");
+  const date = new Date("2024-04-07T23:24:32Z");
+  const timeSinceLastFetch = forTestingOnly.getTimeSinceLastFetch(date, earlyDate);
+
+  const series = testUtils.generateSeries({ lastFetchedAt: date });
+  const syncInterval = 2;
+  const isNew = forTestingOnly.isSeriesFromStorageNew(series, syncInterval, timeSinceLastFetch);
+
+  assertEquals(isNew, false);
+});

lib/caching.ts
@@ -1,21 +1,33 @@
 import { nrkRadio } from "./nrk/nrk.ts";
 import { Series, storage } from "./storage.ts";
-import * as datetime from "datetime";
+import * as datetime from "$std/datetime/mod.ts";
+import { Episode } from "./storage.ts";
 
 const SYNC_INTERVAL_HOURS = 1;
 
-async function initialFetch(options: { id: string }) {
+async function initialFetch(options: { id: string }): Promise<Series | null> {
   const series = await nrkRadio.getSeries(options.id);
+  if (!series) {
+    return null;
+  }
   const stored = storage.write(series);
   if (!stored) {
-    throw new Error(`Failed to store series ${options.id}`);
+    console.error(`Failed to store series ${options.id}`);
+    return null;
   }
 
   return series;
 }
 
-async function updateFetch(existingSeries: Series) {
+type UpdatedSeries = {
+  lastFetch: Date;
+  episodes: Episode[];
+} & Series;
+async function updateFetch(existingSeries: Series): Promise<UpdatedSeries | Series | null> {
   const series = await nrkRadio.getSeries(existingSeries.id);
+  if (!series) {
+    return null;
+  }
   const newEpisodes = series.episodes.filter((episode) => {
     return !existingSeries.episodes.find((serieEpisode) => serieEpisode.id === episode.id);
   });
@@ -24,27 +36,46 @@ async function updateFetch(existingSeries: Series) {
     return existingSeries;
   }
 
+  /**
+   * Since we don't control the API,
+   * we should not make assumptions about the order,
+   * but rather sort the episode to what we want.
+   */
+  const episodesSortedDescending = [...newEpisodes, ...existingSeries.episodes]
+    .sort((a, b) => a.date.getTime() > b.date.getTime() ? -1 : 1);
+
   const updated = {
     ...existingSeries,
     lastFetch: new Date(),
-    /**
-     * Since we don't control the API,
-     * we should not make assumptions about the order,
-     * but rather sort the episode to what we want.
-     */
-    episodes: [...newEpisodes, ...existingSeries.episodes]
-      .sort((a, b) => a.date.getTime() > b.date.getTime() ? -1 : 1),
+    episodes: episodesSortedDescending,
   };
 
   const updateSuccessful = await storage.write(updated);
   if (!updateSuccessful) {
-    throw new Error(`Failed to update series ${existingSeries.id}`);
+    console.log(`Failed to update series ${existingSeries.id}`);
+    return null;
   }
 
   return updated;
 }
 
-async function getSeries(options: { id: string }): Promise<Series> {
+function getTimeSinceLastFetch(inputDate: Date, againstDate = new Date()): number | null {
+  return datetime.difference(inputDate, againstDate, { units: ["hours"] }).hours ?? null;
+}
+
+function isSeriesFromStorageNew(
+  seriesFromStorage: Series,
+  syncInterval = SYNC_INTERVAL_HOURS,
+  timeSinceLastFetch = getTimeSinceLastFetch(seriesFromStorage.lastFetchedAt),
+) {
+  return !Number.isNaN(timeSinceLastFetch) &&
+    seriesFromStorage !== null &&
+    timeSinceLastFetch !== null &&
+    timeSinceLastFetch !== undefined &&
+    timeSinceLastFetch <= syncInterval;
+}
+
+async function getSeries(options: { id: string }): Promise<Series | null> {
   const seriesFromStorage = await storage.read(options);
 
   /**
@@ -56,16 +87,8 @@ async function getSeries(options: { id: string }): Promise<Series> {
     return await initialFetch(options);
   }
 
-  const timeSinceLastFetch =
-    datetime.difference(seriesFromStorage.lastFetchedAt, new Date(), { units: ["hours"] }).hours;
-
   // we have the feed in storage and it's not too old
-  if (
-    seriesFromStorage !== null &&
-    timeSinceLastFetch !== null &&
-    timeSinceLastFetch !== undefined &&
-    timeSinceLastFetch <= SYNC_INTERVAL_HOURS
-  ) {
+  if (isSeriesFromStorageNew(seriesFromStorage)) {
     return seriesFromStorage;
   }
 
@@ -79,4 +102,9 @@ async function getSeries(options: { id: string }): Promise<Series> {
 
 export const caching = {
   getSeries,
+};
+
+export const forTestingOnly = {
+  getTimeSinceLastFetch,
+  isSeriesFromStorageNew,
 };

commit ed0e0b7ab19ecf2967cc3493ca17ddccf46e023e

Fix invalid feed characters
This PR fixes a (sneaky) bug introduced in #25.
XML strings were stringified before being returned to the client, producing invalid XML ("'<></>'" over "<></>").
This PR changes the code such that only JSON responses are stringified.

lib/utils.ts
@@ -14,15 +14,16 @@ type EnumValues<T> = T[keyof T];
 type Status = EnumValues<typeof STATUS_CODE>;
 
 export function responseJSON(body: unknown | null, status: Status) {
-  return response(body, status, "json");
+  const stringifiedBody = JSON.stringify(body);
+  return response(stringifiedBody, status, "json");
 }
 
-export function responseXML(body: unknown | null, status: Status) {
+export function responseXML(body: string, status: Status) {
   return response(body, status, "xml");
 }
 
-function response(body: unknown | null, status: number, type: "json" | "xml") {
-  return new Response(JSON.stringify(body), {
+function response(body: string, status: number, type: "json" | "xml") {
+  return new Response(body, {
     status,
     headers: {
       "Content-Type": `application/${type}`,

routes/api/feeds/[seriesId].ts
@@ -11,6 +11,10 @@ export const handler = async (_req: Request, ctx: FreshContext): Promise<Respons
     return responseJSON({ message: "Series not found" }, STATUS_CODE.NotFound);
   }
   const feed = rss.assembleFeed(series);
+  console.log(feed);
 
-  return responseXML(feed, STATUS_CODE.OK);
+  const xml = responseXML(feed, STATUS_CODE.OK);
+  console.log({ feed, xml });
+
+  return xml;
 };

lib/utils.test.ts
@@ -1,0 +1,16 @@
+import { assertEquals, assertNotEquals } from "$std/assert/mod.ts";
+import { responseJSON, responseXML } from "./utils.ts";
+
+Deno.test("JSON response is stringified", async () => {
+  const body = { message: "Hello, World!" };
+  const response = responseJSON(body, 200);
+  assertEquals(await response.text(), JSON.stringify(body));
+});
+
+Deno.test("XML resopnse is _not_ stringified", async () => {
+  const body = "<message>Hello, World!</message>";
+  const response = responseXML(body, 200);
+  const responseBody = await response.text();
+  assertEquals(responseBody, body);
+  assertNotEquals(responseBody, JSON.stringify(body));
+});