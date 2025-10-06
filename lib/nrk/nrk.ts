a29575e54ec876857ab6793c4fba1273ba54dbf8
Olav Sundfør <olav@sundfoer.com>
2025-10-06 14:11:32 +0200

    Support for seasons (#48)
    Closes https://github.com/olaven/nrss/issues/14.

--- lib/nrk/nrk.ts
+++ lib/nrk/nrk.ts
@@ -1,8 +1,8 @@
 import { get, STATUS_CODE } from "https://deno.land/x/kall@v2.0.0/mod.ts";
-import { components as searchComponents } from "./nrk-search.ts";
+import { Series } from "../storage.ts";
 import { components as catalogComponents } from "./nrk-catalog.ts";
 import { external as playbackComponents } from "./nrk-playback.ts";
-import { Series } from "../storage.ts";
+import { components as searchComponents } from "./nrk-search.ts";
 
 type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
 type PodcastEpisodes = catalogComponents["schemas"]["EpisodesHalResource"];
@@ -10,6 +10,7 @@ type Podcast = catalogComponents["schemas"]["SeriesHalResource"];
 type PodcastEpisodesSingle = catalogComponents["schemas"]["EpisodeHalResource"];
 type RadioSeriesEpisode = catalogComponents["schemas"]["EpisodesHalResource"];
 type RadioSeries = catalogComponents["schemas"]["SeriesHalResource"];
+type SeasonEpisodes = catalogComponents["schemas"]["PodcastSeasonHalResource"];
 
 export type NrkSerie = catalogComponents["schemas"]["SeriesViewModel"];
 export type NrkOriginalEpisode = PodcastEpisodesSingle & { url: string };
@@ -18,11 +19,15 @@ export type NrkSearchResultList = searchComponents["schemas"]["seriesResult"]["r
 export type SearchResult = ArrayElement<NrkSearchResultList> & {
   description?: string;
 };
-export type SeriesData = { episodes: NrkOriginalEpisode[] } & (RadioSeries["series"] | Podcast["series"]);
+export type SeriesData =
+  & { episodes: NrkOriginalEpisode[] }
+  & (
+    | RadioSeries["series"]
+    | Podcast["series"]
+  );
 
 function parseSeries(nrkSeriesData: SeriesData): Series {
   const imageUrl = nrkSeriesData.squareImage?.at(-1)?.url ?? "";
-
   return {
     id: nrkSeriesData.id,
     title: nrkSeriesData.titles.title,
@@ -62,6 +67,41 @@ async function search(query: string): Promise<NrkSearchResultList | null> {
   return null;
 }
 
+async function extractEpisodes(
+  serieResponse: RadioSeries,
+  episodeResponse?: PodcastEpisodes,
+): Promise<NrkOriginalEpisode[]> {
+  if (serieResponse.seriesType === "umbrella") {
+    const seasons = await Promise.all(
+      serieResponse._links.seasons.map(async (season) => {
+        const response = await get<SeasonEpisodes>(
+          `https://psapi.nrk.no${season.href}`,
+        );
+        return response.body;
+      }),
+    );
+
+    const episodes = await Promise.all(
+      seasons.flatMap((season) => {
+        return (
+          season?._embedded.episodes?._embedded.episodes?.flatMap((episode) =>
+            getEpisodeWithDownloadLink(episode, serieResponse.type)
+          ) ?? []
+        );
+      }),
+    );
+
+    return episodes;
+  } else {
+    const episodes = await Promise.all(
+      episodeResponse?._embedded.episodes?.map((episode) => getEpisodeWithDownloadLink(episode, serieResponse.type)) ??
+        [],
+    );
+
+    return episodes;
+  }
+}
+
 async function getSeriesData(seriesId: string): Promise<SeriesData | null> {
   let [
     { status: episodeStatus, body: episodeResponse },
@@ -70,9 +110,7 @@ async function getSeriesData(seriesId: string): Promise<SeriesData | null> {
     get<PodcastEpisodes>(
       `${nrkAPI}/radio/catalog/podcast/${seriesId}/episodes`,
     ),
-    get<Podcast>(
-      `${nrkAPI}/radio/catalog/podcast/${seriesId}`,
-    ),
+    get<Podcast>(`${nrkAPI}/radio/catalog/podcast/${seriesId}`),
   ]);
 
   if (episodeStatus !== STATUS_CODE.OK || seriesStatus !== STATUS_CODE.OK) {
@@ -83,19 +121,17 @@ async function getSeriesData(seriesId: string): Promise<SeriesData | null> {
       get<RadioSeriesEpisode>(
         `https://psapi.nrk.no/radio/catalog/series/${seriesId}/episodes`,
       ),
-      get<RadioSeries>(
-        `https://psapi.nrk.no/radio/catalog/series/${seriesId}`,
-      ),
+      get<RadioSeries>(`https://psapi.nrk.no/radio/catalog/series/${seriesId}`),
     ]);
   }
 
   if (
-    episodeStatus === STATUS_CODE.OK && seriesStatus === STATUS_CODE.OK &&
-    serieResponse?.series && episodeResponse?._embedded.episodes?.length
+    episodeStatus === STATUS_CODE.OK &&
+    seriesStatus === STATUS_CODE.OK &&
+    serieResponse?.series &&
+    episodeResponse?._embedded.episodes?.length
   ) {
-    const episodes = await Promise.all(
-      episodeResponse._embedded.episodes.map((episode) => getEpisodeWithDownloadLink(episode, serieResponse.type)),
-    );
+    const episodes = await extractEpisodes(serieResponse, episodeResponse);
     const seriesData = {
       ...serieResponse.series,
       episodes,
@@ -117,13 +153,18 @@ async function getSeries(seriesId: string): Promise<Series | null> {
   return parsedSeries;
 }
 
-async function getEpisode(seriesId: string, episodeId: string): Promise<NrkPodcastEpisode | null> {
+async function getEpisode(
+  seriesId: string,
+  episodeId: string,
+): Promise<NrkPodcastEpisode | null> {
   const url = `${nrkAPI}/radio/catalog/podcast/${seriesId}/episodes/${episodeId}`;
   const { status, body: episode } = await get<NrkPodcastEpisode>(url);
   if (status === STATUS_CODE.OK && episode) {
     return episode;
   }
-  console.error(`Error getting episode ${episodeId}. Status: ${status}. Series: ${seriesId}`);
+  console.error(
+    `Error getting episode ${episodeId}. Status: ${status}. Series: ${seriesId}`,
+  );
   return null;
 }
 
