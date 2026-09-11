8d6b7736d992652d1a10d13a9471bf12650f068b
olaven <olav@sundfoer.com>
2026-09-12 00:37:07 +0200

    remove logging of schema

--- lib/storage/connection-options.ts
+++ lib/storage/connection-options.ts
@@ -2,7 +2,6 @@ import "jsr:@std/dotenv/load";
 
 export const schema = Deno.env.get("PGSCHEMA") 
 
-console.log({schema})
 export const connectionOptions = {
   host: Deno.env.get("PGHOST"),
   port: Number(Deno.env.get("PGPORT") ?? 5432),
